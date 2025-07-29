f556760fd0115b60b7abc6a7ad5e6c2b82932e9d
EKennard <lizz.kennard@hotmail.com>
2025-07-29 19:57:35 +0100

    child form added to forms.py

--- planner/forms.py
+++ planner/forms.py
@@ -2,16 +2,37 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Parent, Child, Entry, Category
+import random
 
-class RegistrationForm(UserCreationForm):
+# generates random colour code for the child model
+def generate_random_color():
+    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+class registrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
     def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
+        user = super(registrationForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
+            Parent.objects.create(user=user)
         return user
+
+class childForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.instance.pk:
+            self.fields['colour'].initial = generate_random_color()
+    
+    class Meta:
+        model = Child
+        fields = ['name', 'birth_date', 'school', 'year', 'class_name', 'colour']
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+            'colour': forms.TextInput(attrs={'type': 'color'}),
+        }
+
